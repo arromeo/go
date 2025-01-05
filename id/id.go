18c86e4d263d18aefaad1c742f41e04d0b680d83
Adam Romeo <adam.r.romeo@gmail.com>
2025-01-05 22:28:35 +0000

    [id] Implement GenerateId function

--- id/id.go
+++ id/id.go
@@ -1,5 +1,20 @@
 package id
 
+import (
+	"crypto/rand"
+	"math/big"
+)
+
 func GenerateId() string {
-	return "Some ID"
+	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+	const length = 8
+	id := make([]byte, length)
+	for i := range id {
+		randomByte, err := rand.Int(rand.Reader, big.NewInt(int64(len(charset))))
+		if err != nil {
+			panic(err)
+		}
+		id[i] = charset[randomByte.Int64()]
+	}
+	return string(id)
 }
